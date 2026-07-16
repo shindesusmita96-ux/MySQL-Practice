b41b0376f136ee80e2dc0f7229149cbeaa1a0f93
shindesusmita96-ux <shindesusmita96@gmail.com>
2026-07-16 09:27:28 +0530

    Update Useful_Queries.sql

--- Useful_Queries.sql
+++ Useful_Queries.sql
@@ -1,4 +1,7 @@
--- Find inconsistent values ignoring case and accents
+-- =======================================================================================================================================
+-- Query: Find Inconsistent Text Values (Case & Accent Variations)
+-- Purpose: This query identifies text values that appear to be the samewhen ignoring differences in letter casing and accent marks.
+-- =======================================================================================================================================
 
 SELECT
     city COLLATE Latin1_General_CI_AI,
@@ -6,3 +9,12 @@ SELECT
 FROM orders
 GROUP BY city COLLATE Latin1_General_CI_AI
 HAVING COUNT(DISTINCT city) > 1;
+
+/*
+CI = Case Insensitive
+AI = Accent Insensitive
+
+Example:
+'NEW YORK' = 'new york'
+'São Paulo' = 'Sao Paulo'
+*/
